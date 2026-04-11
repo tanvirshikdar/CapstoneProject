31b62130547e8c2ee028c90b849307bd5898d548
Tanvir Shikdar <tanvir.shikdar@pm.me>
2026-04-11 19:56:11 +0600

    Second Last Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlides(n) {
+    if (n >= slides.length) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = n;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[slideIndex].classList.add("active");
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex + n);
+}
+
+setInterval(() => {
+    changeSlide(1);
+}, 1000);
+
+let currentMenuIndex = 0;
+const menuPages = document.querySelectorAll(".menu-page");
+
+function showMenuPage(n) {
+    if (menuPages.length === 0) return;
+
+    if (n >= menuPages.length) {
+        currentMenuIndex = 0;
+    }
+    else if (n < 0) {
+        currentMenuIndex = menuPages.length - 1;
+    }
+    else {
+        currentMenuIndex = n;
+    }
+    menuPages.forEach(page => {
+        page.classList.remove("active");
+    })
+    menuPages[currentMenuIndex].classList.add("active");
+}
+
+function changeMenuPage(n) {
+    showMenuPage(currentMenuIndex + n);
+}
+
